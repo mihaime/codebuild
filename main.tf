6167c25886bbc60badd3788f8a7b671f373d5d77
Mihai <mtanasescu@aviatrix.com>
2021-11-15 15:26:59 +0100

    o laie

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "aviatrixcontroller" {
+  source            = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-build?ref=terraform_0.14"
+  vpc               = "abc"
+  subnet            = "subnet-9x3237xx"
+  keypair           = "keypairname"
+  ec2role           = "aviatrix-role-ec2"
+  incoming_ssl_cidr = ["0.0.0.0/0"]
+}
