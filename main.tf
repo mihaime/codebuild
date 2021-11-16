1a6a744060669cc008f94a019584c8cc19256431
Mihai <mtanasescu@aviatrix.com>
2021-11-16 16:06:12 +0100

    bla bla

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "iam_roles" {
 # Create VPC
 
 # Create Controller
-
+# Logs with CloudWatch?
 /*
 The AWS account ID where the Aviatrix Controller was/will be launched. This is only required if you are creating roles for the secondary account different from the account where controller was/will be launched. DO NOT use this parameter if this Terraform module is applied on the AWS account of your controller.
 
