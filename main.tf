ef60de9741cd318724ab4e8de7b2322ad09eadcc
Mihai <mtanasescu@aviatrix.com>
2021-11-16 14:05:16 +0100

    bla bla

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
-module "aviatrixcontroller" {
-  source            = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-build?ref=terraform_0.14"
-  vpc               = "abcd"
-  subnet            = "subnet-9x3237xx"
-  keypair           = "keypairname"
-  ec2role           = "aviatrix-role-ec2"
-  incoming_ssl_cidr = ["0.0.0.0/0"]
+/* IAM ROLES */
+
+data "aws_ssm_parameter" "awsctrlaccount" {
+  name = "cspaccount"
 }
 
-/*
-something else
+data.aws_ssm_parameter.awsctrlaccount.value
+
+module "iam_roles" {
+  source = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-iam-roles?ref=terraform_0.14"
+}
+
+/* 
+The AWS account ID where the Aviatrix Controller was/will be launched. This is only required if you are creating roles for the secondary account different from the account where controller was/will be launched. DO NOT use this parameter if this Terraform module is applied on the AWS account of your controller.
+
+Same definition using als extra param this for the latter case:
+  external-controller-account-id = ""
+
 */
+
