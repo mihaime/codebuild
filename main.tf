58c0ddfc7100650c4173fa6afe1fe21064d344c6
Mihai <mtanasescu@aviatrix.com>
2021-11-16 16:01:23 +0100

    bla bla

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ data "aws_ssm_parameter" "awsctrlaccount" {
   name = "cspaccount"
 }
 
+# Creates
 # aviatrix-role-ec2
 # aviatrix-role-app
 
@@ -14,7 +15,6 @@ module "iam_roles" {
 }
 
 /*
-fdfsd
 The AWS account ID where the Aviatrix Controller was/will be launched. This is only required if you are creating roles for the secondary account different from the account where controller was/will be launched. DO NOT use this parameter if this Terraform module is applied on the AWS account of your controller.
 
 Same definition using als extra param this for the latter case:
---
+++ providers.tf
@@ -0,0 +1,11 @@
+/*
+provider "aviatrix" {
+  controller_ip = var.aviatrix_controller_ip
+  username      = var.aviatrix_admin_account
+  password      = var.aviatrix_admin_password
+}
+*/
+
+provider "aws" {
+  region = "eu-west-3"
+}
