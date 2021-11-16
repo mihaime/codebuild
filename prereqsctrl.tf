7cbda20d2eb746275712c0c4db629b0bbbb1e27f
Mihai <mtanasescu@aviatrix.com>
2021-11-16 17:21:00 +0100

    bla bla

--- prereqsctrl.tf
+++ prereqsctrl.tf
@@ -1,5 +1,11 @@
 /* Fetch Using: data.aws_ssm_parameter.awsctrlaccount.value */
 
+module "avx_ctrl_key" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name   = "avx_ctrl_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9lhwk6x8eC+XDT8c0NQr4mxGfCKqCdfWqGkIzaC952Vg/6TKe/YpuiWibMVMUoRg8jm3jFXExzNZoKA86yEDEo4n9u5FVQA7UiFEEMd7fjjwFOQI60K7hgJjEUY/puqVobRnNAXUSk7bfqYAYOw6QK+DLu3z7jK1Mjcb7LJVQPWWZLq1jVSosF5drPbbEo++m2UstA8ZZHewGYdecs6SieeZjcovOrlQv9NONHjiszbN69zyuTcFJheT+U7opadz3WyI8A9zW//bp238+F5pPK+5dBY0+DQHEzGx1XCZzZJ+8mKULbJAb2q3oZG7S+AJ8jABKjCHNHxdZIVf/tHpD3WgqpRDRj6XEQFyXksTNKl+LC/gZmxxddlDMkNab4ZJqUesz0JBlgfV4z9w4Y3EagA1uQmcM8okQZjSq3akfEbhiApN1yiPFAlxTMVgVyqdlNe/kWUoVVbohOFKPVjU/tDaMSQj6iuSVzFONwckFczzTefEhIJdmyP5YxFQWSqeuIXmlpJOswVjnnqgiOijiavZ1dgo1kGhXI9GmZX6ZgvrEcPcNpp20Jrtey2QHssAOxjf1ndq8vydf64kE68rLQLL0sMulcbbW2DmkSTkMOAM5NM9ZBlCRh7ovW88zRsI+EENrINorpBN6Fvlllydppgrabv1WW+4cTFOZVmdYtw== mihaitanasescu@Mihais-MacBook-Pro.local"
+}
 
 resource "aws_vpc" "avtx_ctrl_vpc" {
   cidr_block       = data.aws_ssm_parameter.awsctrlcidr.value
