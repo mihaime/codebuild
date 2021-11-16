3c86848e045accfb34a9735787906672f2b94fcf
Mihai <mtanasescu@aviatrix.com>
2021-11-16 15:29:28 +0100

    bla bla

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "iam_roles" {
   source = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-iam-roles?ref=terraform_0.14"
 }
 
-/* oh crap
+/* oh crap again?
 The AWS account ID where the Aviatrix Controller was/will be launched. This is only required if you are creating roles for the secondary account different from the account where controller was/will be launched. DO NOT use this parameter if this Terraform module is applied on the AWS account of your controller.
 
 Same definition using als extra param this for the latter case:
