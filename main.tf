4a6a11a860dc7dcdd77193ddfba21b7ee41d915f
Mihai <mtanasescu@aviatrix.com>
2021-11-17 00:57:06 +0100

    bla bla

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "iam_roles" {
 module "aviatrixcontroller" {
   source            = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-build?ref=terraform_0.14"
   vpc               = aws_vpc.avtx_ctrl_vpc.id
-  subnet            = aws_subnet.avtx_ctrl_subnet_a.public_subnet_id
+  subnet            = aws_subnet.avtx_ctrl_subnet_a.id
   keypair           = module.avx_ctrl_key.key_name
   ec2role           = module.iam-roles.aviatrix-role-ec2-name
   incoming_ssl_cidr = ["0.0.0.0/0"]
